e30ff13a02aec7710c217713eaf00782f2cd039e
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-13 18:54:08 +0000

    added demo text paragraph

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
 <h1>about</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, beatae enim provident quisquam tempore tenetur. Aperiam, asperiores expedita id nostrum officia rem tempora! Atque beatae blanditiis dolore maxime, perspiciatis rem.</p>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>home page</h1>
+<h1>home</h1>
+
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolorem dolores exercitationem ipsa, nemo nulla numquam quasi quia. Esse non possimus quasi veritatis? Ea harum, incidunt inventore nisi temporibus ullam.
+</p>
 @endsection
 
 
