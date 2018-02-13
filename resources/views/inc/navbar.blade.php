f8a3a17b707bf37429946e0bedfc47e153c5a3e4
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-13 20:30:24 +0000

    added shorthand if stamtement for navbar active url

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -7,17 +7,22 @@
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
 
-          <li class="nav-item active">
-            <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
+
+          <li class="{{Request::is('/') ? 'nav-item active' : '' }}">
+            <a class="nav-link" href="/home">Home</a>
           </li>
 
-          <li class="nav-item">
+          <li class="{{Request::is('about') ? 'nav-item active' :'' }}">
             <a class="nav-link" href="/about">About</a>
           </li>
 
-          <li class="nav-item">
-            <a class="nav-link contact" href="/contact">Contact</a>
+          <li class="{{Request::is('contact') ? 'nav-item active':'' }}">
+            <a class="nav-link" href="/contact">Contact</a>
           </li>
+
+        <li class="{{Request::is('messages') ? 'nav-item active':'' }}">
+          <a class="nav-link" href="/messages">Messages</a>
+        </li>
 
 
           <li class="nav-item">
